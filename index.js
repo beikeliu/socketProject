69d617dda7564edbeb296ec09290616e18fa2a69
liubeike <liubeike99@gmail.com>
2025-02-17 21:43:55 +0800

    add log record

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ const messageSchema = new mongoose.Schema({
     timestamp: { type: Date, default: Date.now }
 });
 const Message = mongoose.model('Message', messageSchema);
+const logSchema = new mongoose.Schema({
+    username: String,
+    message: String,
+    timestamp: { type: Date, default: Date.now }
+});
+const Logg = mongoose.model('Logg', logSchema);
 
 async function saveMessage(username, message) {
     const msg = new Message({ username, message });
     await msg.save();
+}
+
+async function saveLogg(username, message) {
+    const logg = new Logg({ username, message });
+    await logg.save();
 }
 
 async function getRecentMessages() {
@@ -41,15 +52,11 @@ io.on('connection', async (socket) => {
     io.emit('chat history', messages);
     socket.on('join user', async (data) => {
         socket.username = data.username;
-        // data.message = '加入了聊天室';
-        // await saveMessage(data.username, data.message);
-        // io.emit('chat message', { ...data, timestamp: new Date().toJSON() });
+        await saveLogg(data.username, '加入了聊天室');
     });
     socket.on('disconnect', async (data) => {
         io.emit("user count", io.sockets.sockets.size);
-        // const message = '离开了聊天室';
-        // await saveMessage(socket.username, message);
-        // io.emit('chat message', { username: socket.username, message, timestamp: new Date().toJSON() });
+        await saveLogg(socket.username, '离开了聊天室');
     });
     socket.on('chat message', async (data) => {
         await saveMessage(data.username, data.message);
